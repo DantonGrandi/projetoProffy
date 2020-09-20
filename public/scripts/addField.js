ce907daf007334b0c56331bcf4992ee857ccaab9
danton <danton_grandi@hotmail.com>
2020-09-20 17:56:58 -0300

    dia 4 de code

---
+++ public/scripts/addField.js
@@ -0,0 +1,24 @@
+// procurar o botao
+document.querySelector("#add-time")
+//quando clicar no botao
+.addEventListener('click', cloneField)
+
+// Executar uma acao
+function cloneField() {
+    //Duplicar os campos. que campos?
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
+
+    //pegar os campos. Que campos?
+    const fields = newFieldContainer.querySelectorAll("input")
+    
+
+    //para cada campo, limpar
+    fields.forEach(function(field) {
+        field.value = ""
+    })
+
+    //colocar na pagina: onde?
+    document.querySelector("#schedule-items").appendChild(newFieldContainer)
+
+}
+    
